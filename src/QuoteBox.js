e8dcc3580af7a7d732fe2585887c8835d4479777
Erik <kiredroffas@github.com>
2020-09-22 20:53:53 -0700

    New quote button now causes intended color change behavior, small CSS tweaks

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -4,43 +4,59 @@ import './QuoteBox.css';
 class QuoteBox extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
-        //this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            background: '#'+Math.floor(Math.random()*16777215).toString(16),
+            quote: 'Do or do not, there is no try.',
+            author: 'Yoda man'
+        };
+        this.newQuote = this.newQuote.bind(this);
     }
 
     componentDidMount() {
-        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = this.state.background;
+    }
+
+
+    newQuote() {
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        this.setState({
+            background: randomColor,
+        }, () => {
+            document.body.style.backgroundColor = this.state.background;
+        }); 
     }
 
     render() {
-        const quote = 'Do or do not, there is no try.';
-        const author = 'Yoda man';
-
         return (
             <div id="wrapper">
-                <div id="quote-box">
+                <div id="quote-box" style={{color: this.state.background}}>
                     <div className="blockquote text-center" id="text">
-                        "{quote}"
+                        "{this.state.quote}"
                     </div>
+                    <br />
                     <div className="text-right" id="author">
-                        -{author}
+                        -{this.state.author}
                     </div>
                     <br/>
                     <br />
-                    <div className="row">
+                    <div className="row align-items-center">
                         <div className="col-sm-4">
-                            <a id="tweet-quote" href="https://twitter.com/intent/tweet">
-                                <i className="twitter icon" style={{fontSize: '2rem'}}></i>
-                            </a>
+                            <button type="button" className="btn" id="tweet-button">
+                                <a id="tweet-quote" href="https://twitter.com/intent/tweet">
+                                    <i className="twitter icon" style={{fontSize: '1.2rem', color: 'white'}}></i>
+                                </a>
+                            </button>
                         </div>
                         <div className="col-sm-4"></div>
                         <div className="col-sm-4">
-                            <button type="button" className="btn" id="new-quote">
+                            <button type="button" className="btn" id="new-quote" onClick={this.newQuote}>
                                 New Quote
                             </button>
                         </div>
                     </div> 
                 </div>
+                <br />
+                <p>By <em>Kiredroffas</em></p>
             </div>
         );
     }
